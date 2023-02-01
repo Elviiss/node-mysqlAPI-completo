4a41189b03f573242d3968066330984570608133
Elviiss <elvis22elmejor@gmail.com>
2023-02-01 13:58:38 +0100

    Endpoints funcionales al inicio

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', (req, res, next) =>{
+  res.send("Hola mundo");
+});
+
+router.get('/employees', (req, res, next) => {
+  res.send("Obteniendo empleados");
+});
+
+router.post('/employees', function(req, res, next) {
+  res.send("Creando empleados");
+});
+
+router.put('/employees', function(req, res, next) {
+  res.send("Actualizando empleado");
+});
+
+router.delete('/employees', function(req, res, next) {
+  res.send("Eliminando empleados");
+});
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("Hola mundo");
 });
 
 module.exports = router;
