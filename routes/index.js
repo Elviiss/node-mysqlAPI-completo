fd63fc6bcaeaeea91a9d2a1e2c63bd64681cadda
Elviiss <elvis22elmejor@gmail.com>
2023-02-01 14:21:57 +0100

    conexion a bd correct

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../db.js')
 
 /* GET home page. */
 router.get('/', (req, res, next) =>{
-  res.send("Hola mundo");
+  res.send("HOLA MUNDO");
+});
+
+router.get('/ping', async(req, res, next) =>{
+  const result = await pool.query("SELECT 1 + 1 AS result")
+  res.json(result[0]);
 });
 
 router.get('/employees', (req, res, next) => {
